de244f6e2dca4b45b421e7b93978e7527171f281
Steven Liao <nx01@N-X---0-6---SL.local>
2025-08-21 17:27:18 -0400

    Remove nested node_modules and fix .gitignore

--- src/.next/server/app/favicon.ico/route.js
+++ src/.next/server/app/favicon.ico/route.js
@@ -1,6 +1,6 @@
 var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
-R.c("server/chunks/929f9_next_f81ade1a._.js")
-R.c("server/chunks/[root-of-the-server]__ac3b7f9b._.js")
-R.m("[project]/intake-assessment-v2/src/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/intake-assessment-v2/src/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/intake-assessment-v2/src/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)")
-module.exports=R.m("[project]/intake-assessment-v2/src/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/intake-assessment-v2/src/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)").exports
+R.c("server/chunks/node_modules_next_6bb3a9b9._.js")
+R.c("server/chunks/[root-of-the-server]__147c551c._.js")
+R.m("[project]/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
+R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)").exports
