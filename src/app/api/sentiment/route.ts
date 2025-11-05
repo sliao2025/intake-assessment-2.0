49a7ef3f50f39b5905384d5352411196a8c065f2
Steven Liao <nx01@N-X---0-6---SL.local>
2025-11-05 13:58:50 -0500

    new

--- src/app/api/sentiment/route.ts
+++ src/app/api/sentiment/route.ts
@@ -2,10 +2,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../lib/prisma";
 import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function POST(request: NextRequest) {
   try {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
     const userId = session?.user?.id;
 
     if (!userId) {
